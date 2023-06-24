538dbc25b45f359a0a0ac9dde4091db12b078d41
sankavi10 <136688954+sankavi10@users.noreply.github.com>
2023-06-24 17:58:50 +0530

    Add files via upload

---
+++ sankavi (1).py
@@ -0,0 +1,19 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python311/myfirstprogrammukilan.py
+Hello, World!
+>>> 
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python311/myfirstprogrammukilan.py
+Hello, World!
+What is your name?mukilan
+Hello mukilan
+>>> age=15
+>>> print(age)
+15
+>>> weight=50
+>>> print(weight)
+50
+>>> height=154
+>>> print(height)
+154
