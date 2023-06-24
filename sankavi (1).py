e67e469a35c9dbb4c42f22f00aa96dc2650e441f
sankavi10 <136688954+sankavi10@users.noreply.github.com>
2023-06-24 18:05:54 +0530

    Update sankavi (1).py

--- sankavi (1).py
+++ sankavi (1).py
@@ -1,13 +1,13 @@
 Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python311/myfirstprogrammukilan.py
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python311/myfirstprogramsankavi.py
 Hello, World!
 >>> 
-= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python311/myfirstprogrammukilan.py
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python311/myfirstprogramsankavi.py
 Hello, World!
-What is your name?mukilan
-Hello mukilan
+What is your name?sankavi
+Hello sankavi
 >>> age=15
 >>> print(age)
 15
